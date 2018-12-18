bd4ce4d8abe1cfc420718aba273c01d441f727bb
Luka <lukamartinovic@gmail.com>
2018-12-18 21:01:15 +0100

    Task 3a - Pretraga vozila

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-const db = require('../dbsetup');
+const cars = require('../middlewares/cars');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+router.get('/cars/search', cars.search);
 
 module.exports = router;
